440ef776fbf55b64e2f1d84def4a438ccb6e97d4
mayconrfreitas <mayconrfreitas@gmail.com>
2023-07-16 23:39:27 -0300

    formatação informações de tentativas no log

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 /**
  * Código retirado do vídeo: https://www.youtube.com/watch?v=ztQEaQ06GYs&ab_channel=LucasSantos
- * 
+ *
  * Para rodar:
- * 1. Caso tenho faito alguma modificação no código, execute o comando: npm run build	
+ * 1. Caso tenho faito alguma modificação no código, execute o comando: npm run build
  * 2. Execute o comando: npm start -- [dificuldade] [numBlocos]
  * 		Exemplo: npm start -- 4 10
- * 
+ *
  */
 
 
@@ -18,9 +18,9 @@ const numBlocos  = Number(process.argv[3]) || 10;
 let chain = blockchain.chain;
 
 for (let i = 1; i <= numBlocos; i++) {
-	const bloco = blockchain.criarBloco(`Bloco ${i}`);
-	const mineInfo = blockchain.minerarBloco(bloco);
-	chain = blockchain.enviarBloco(mineInfo.blocoMinerado);
+  const bloco = blockchain.criarBloco(`Bloco ${i}`);
+  const mineInfo = blockchain.minerarBloco(bloco);
+  chain = blockchain.enviarBloco(mineInfo.blocoMinerado);
 }
 
 // console.log(`--- BLOCKCHAIN ---`);
